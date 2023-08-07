25b3c9615813786eba2bd6362d709f031fcde709
kishiguro <kunihiro@zebra.dev>
2023-08-07 08:41:37 -0700

    Support Password Confirm feature when creating new user.

--- frontend/src/pages/UserCreate.tsx
+++ frontend/src/pages/UserCreate.tsx
@@ -6,17 +6,57 @@ import axios from "../axios";
 import { User } from "../api/api";
 
 import Dashboard from "../Dashboard";
-import { Button, Grid, TextField, Table, TableBody, TableCell, TableRow } from "@mui/material";
+import {
+  Button,
+  Grid,
+  InputAdornment,
+  IconButton,
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+export interface Password {
+  password?: string;
+  passwordConfirm?: string;
+}
 
 export default function UserCreate() {
   const navigation = useNavigate();
   const [user, setUser] = useState<User>({});
+
+  const [password, setPassword] = useState<Password>({});
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+  const handleClickShowPasswordConfirm = () => setShowPasswordConfirm(!showPasswordConfirm);
+  const handleMouseDownPasswordConfirm = () => setShowPasswordConfirm(!showPasswordConfirm);
 
   const { id } = useParams<{
     id: string;
   }>();
 
   const handleCreate = () => {
+    if (password.password === undefined || password.password === "") {
+      alert("Password can't be empty");
+      return;
+    }
+    if (password.passwordConfirm === undefined || password.passwordConfirm === "") {
+      alert("Password can't be empty");
+      return;
+    }
+    if (password.password !== password.passwordConfirm) {
+      alert("Password mismatch");
+      return;
+    }
+    user.encryptedPassword = password.password;
     axios
       .post("/api/tenant/" + id + "/user", user)
       .then((res) => {
@@ -37,7 +77,13 @@ export default function UserCreate() {
   const handleChangePassword = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ): void => {
-    setUser({ ...user, encryptedPassword: event.target.value });
+    setPassword({ ...password, password: event.target.value });
+  };
+
+  const handleChangePasswordConfirm = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ): void => {
+    setPassword({ ...password, passwordConfirm: event.target.value });
   };
 
   return (
@@ -65,8 +111,48 @@ export default function UserCreate() {
                 variant="outlined"
                 required
                 fullWidth
-                value={user.encryptedPassword}
+                type={showPassword ? "text" : "password"}
                 onChange={handleChangePassword}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+				    {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <TextField
+                label="Confirm Password"
+                variant="outlined"
+                required
+                fullWidth
+                type={showPasswordConfirm ? "text" : "password"}
+                onChange={handleChangePasswordConfirm}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPasswordConfirm}
+                        onMouseDown={handleMouseDownPasswordConfirm}
+                      >
+				    {showPasswordConfirm ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
             </TableCell>
           </TableRow>
