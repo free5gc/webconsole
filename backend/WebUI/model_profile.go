fb313a9bc0f25a0e6e4dedd46119bc9e8abfd214
Alonza0314 <dufengplus@gmail.com>
2024-10-15 15:18:05 +0800

    feat: add profile model

---
+++ backend/WebUI/model_profile.go
@@ -0,0 +1,15 @@
+package WebUI
+
+import "github.com/free5gc/openapi/models"
+
+type Profile struct {
+	ProfileName                       string                                     `json:"profileName"`
+	AccessAndMobilitySubscriptionData models.AccessAndMobilitySubscriptionData   `json:"AccessAndMobilitySubscriptionData"`
+	SessionManagementSubscriptionData []models.SessionManagementSubscriptionData `json:"SessionManagementSubscriptionData"`
+	SmfSelectionSubscriptionData      models.SmfSelectionSubscriptionData        `json:"SmfSelectionSubscriptionData"`
+	AmPolicyData                      models.AmPolicyData                        `json:"AmPolicyData"`
+	SmPolicyData                      models.SmPolicyData                        `json:"SmPolicyData"`
+	FlowRules                         []FlowRule                                 `json:"FlowRules"`
+	QosFlows                          []QosFlow                                  `json:"QosFlows"`
+	ChargingData                      []ChargingData                             `json:"ChargingData"`
+}
